44d081ba6e5b48c7a29f9113629d60303cd63fe1
j <juliocadima29@gmail.com>
2024-09-23 23:23:52 -0400

    se añado el servicio vacio de descuento

---
+++ M7Tarea1.Server/Services/ServicioDescuento.cs
@@ -0,0 +1,14 @@
+﻿
+using M7Tarea1.Server.Data;
+using M7Tarea1.Server.Data.Models;
+
+namespace M7Tarea1.Server.Services
+{
+    public class ServicioDescuento : IServicio
+    {
+        public Task Registrar(object item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
